34dacb019f45c73a5c6f63b7fa2887639f254bac
Jon Preece <jonpreece@hotmail.co.uk>
2019-01-05 08:44:34 +0000

    Add Google Adsense code

--- src/html.js
+++ src/html.js
@@ -39,10 +39,13 @@ function HTML(props) {
         {jquery}
         {popper}
         {bootstrap}
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
         <script
-          type="text/javascript"
           dangerouslySetInnerHTML={{
-            __html: `window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us7.list-manage.com","uuid":"59dc75620baddb24611cddb4a","lid":"cb867e2fbf","uniqueMethods":true}) })`
+            __html: ` (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-7672048829297382",
+          enable_page_level_ads: true
+     })`
           }}
         />
       </body>
