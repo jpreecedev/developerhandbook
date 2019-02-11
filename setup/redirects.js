aaca93e626fbedd04910244672b84155f19c46da
Jon Preece <jonpreece@hotmail.co.uk>
2019-02-11 18:47:09 +0000

    Missing links

--- setup/redirects.js
+++ setup/redirects.js
@@ -2,6 +2,18 @@ const Redirects = [
   {
     from: '/career/devs-this-is-how-i-got-fit/null',
     to: '/career/devs-this-is-how-i-got-fit/'
+  },
+  {
+    from: '/c-sharp/create-restful-api-authentication-using-web-api-jwt/null',
+    to: '/c-sharp/create-restful-api-authentication-using-web-api-jwt/'
+  },
+  {
+    from: '/entity-framework/',
+    to: '/category/entity-framework/'
+  },
+  {
+    from: '/wpf-mvvm/',
+    to: '/category/wpf-mvvm/'
   }
 ]
 
