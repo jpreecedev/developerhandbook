9c94cdb51a9004a6e1a095298b429ac9be0309fd
Jon Preece <jonpreece@hotmail.co.uk>
2019-03-17 06:35:02 +0000

    Try fix redirects not working

--- setup/redirects.js
+++ setup/redirects.js
@@ -1,12 +1,22 @@
 const Redirects = require('./redirect-urls.json')
 
 function getRedirects() {
-  return Redirects.map(({ from, to }) => ({
-    fromPath: from,
-    toPath: to,
-    isPermanent: true,
-    redirectInBrowser: true
-  }))
+  return Redirects.map(({ from, to }) => {
+    return [
+      {
+        fromPath: from,
+        toPath: to,
+        isPermanent: true,
+        redirectInBrowser: true
+      },
+      {
+        fromPath: from.substr(0, from.length - 1),
+        toPath: to,
+        isPermanent: true,
+        redirectInBrowser: true
+      }
+    ]
+  }).flat()
 }
 
 module.exports = getRedirects
