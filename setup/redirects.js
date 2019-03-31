f750832c5f7f5e850332d8551cc1ec2073a9ef26
Jon Preece <jonpreece@hotmail.co.uk>
2019-03-31 17:29:35 +0100

    Added redirects for URLs with null

--- setup/redirects.js
+++ setup/redirects.js
@@ -10,6 +10,12 @@ function getRedirects() {
         redirectInBrowser: true
       },
       {
+        fromPath: `${from}null`,
+        toPath: to,
+        isPermanent: true,
+        redirectInBrowser: true
+      },
+      {
         fromPath: from.substr(0, from.length - 1),
         toPath: to,
         isPermanent: true,
