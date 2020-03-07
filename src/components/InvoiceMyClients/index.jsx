4b7dc5cc01c29d4a480c7226bda160b209b7161e
Jon Preece <jonpreece@hotmail.co.uk>
2020-03-07 06:47:43 +0000

    feat(invoicemyclients): update copy

--- src/components/InvoiceMyClients/index.jsx
+++ src/components/InvoiceMyClients/index.jsx
@@ -4,19 +4,13 @@ const InvoiceMyClients = () => {
   return (
     <section id="invoicemyclients" className="invoicemyclients">
       <h3>Attention! We're launching a new service!</h3>
-      <p>Are you a freelancer who has trouble getting your clients to pay on time?</p>
-      <p>Time and time again we have had to chase clients to pay outstanding invoices.</p>
-      <p>We love our clients but we have to make ends meet.</p>
-      <p className="margin large">
-        We will soon launch a new service which will help our clients pay outstanding
-        invoices in a timely manner, as well as help us, the freelancer, understand which
-        invoices have been paid and what's outstanding.
+      <p>
+        Are you a freelancer? We're building a completely free new service that makes it
+        quick and easy for your clients to pay their invoices.
       </p>
-      <p>Did we mention that it's free?</p>
       <a className="btn btn-white" href="https://invoicemyclients.com/landing">
-        Register your interest today
+        Find out more
       </a>
-      <small>P.S. sorry for the interruption!</small>
     </section>
   )
 }
